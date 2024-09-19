5d94b8518d5abfbf5f32d2d24a8f0b0296202eaa
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-09-19 21:11:44 +0200

    Create functions to display button values Make calculator work for operators

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let isOperatorClicked = false; 
 
 const add = (a, b) => a + b;
 
@@ -9,6 +10,13 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const pageContainer = document.querySelector(".page-container");
+const calcContainer = document.querySelector(".calc-container");
+const displayContainer = document.querySelector(".display-container");
+const keypadContainer = pageContainer.querySelector(".keypad-container");
+const displayButton = displayContainer.querySelector(".display-button");
+const equal = keypadContainer.querySelector(".equal")
 
 const operate = (a, b, oper) => {
   switch (oper) {
@@ -21,19 +29,18 @@ const operate = (a, b, oper) => {
     case "/":
       return divide(a, b);
   }
-};
-const pageContainer = document.querySelector(".page-container");
-const calcContainer = document.querySelector(".calc-container");
-const displayContainer = document.querySelector(".display-container");
-const keypadContainer = pageContainer.querySelector(".keypad-container");
-const displayButton = displayContainer.querySelector(".display-button");
+ }
 
-let numDisplayValue = "";
-let operDisplayValue = "";
+ equal.addEventListener("click", () => {
+  const a = parseInt(firstNumber)
+  const b = parseInt(secondNumber)
+  const result = operate(a, b, operator)
+  displayButton.textContent = result;
+  console.log(result)
+ })
 
-const updateDisplay = () => {
-  displayButton.textContent = numDisplayValue;
-};
+
+
 
 const displayNumbers = () => {
   const buttons = keypadContainer.querySelectorAll(".operand");
@@ -42,29 +49,39 @@ const displayNumbers = () => {
     butt.addEventListener("click", (e) => {
       const buttonValue = e.target.value;
       console.log("button value:", buttonValue);
-      numDisplayValue += buttonValue;
-      console.log("display value:", numDisplayValue);
-      firstNumber = numDisplayValue
-      updateDisplay();
+
+      if (!isOperatorClicked) {
+         firstNumber += buttonValue;
+      displayButton.textContent = firstNumber;
+      console.log("display (first #)", firstNumber);
+      } else {
+        displayButton.textContent = "";
+
+        secondNumber += buttonValue;
+        displayButton.textContent = secondNumber;
+        console.log("display (second #)", secondNumber);
+      }
+     
     });
   });
 };
 
 const displayOperator = () => {
-  const operators = keypadContainer.querySelectorAll(".operator")
+  const operators = keypadContainer.querySelectorAll(".operator");
   operators.forEach((oper) => {
     oper.addEventListener("click", (e) => {
-      const operatorValue = e.target.value
-      console.log(operatorValue)
-      operDisplayValue = operatorValue
-      console.log("display value:", operDisplayValue);
-    })
-  })
-}
 
+      const operatorValue = e.target.value;
+      console.log("operator value:", operatorValue);
+      operator = operatorValue;
+      isOperatorClicked = true;
 
+      displayButton.textContent = operator;
+      console.log("display value:", operator);
 
-
+    });
+  });
+};
 
 displayNumbers();
-displayOperator()
+displayOperator();
