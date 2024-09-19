3d088788348b1ad468b0642272c93dce2bc3e5c0
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-09-19 11:55:16 +0200

    Create basic HTML calculator

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const add = (a, b) => {
-return a + b
-}
+let firstNumber;
+let secondNumber;
+let operator;
 
+const add = (a, b) => a + b;
 
-const subtract = (a, b) => {
-  return a - b
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const operate = (a, b, oper) => {
+  switch (oper) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
   }
+};
 
-  const multiply = (a, b) => {
-    return a * b
-    }
-
-    const divide = (a, b) => {
-      return a / b
-      }
-
+console.log(operate(355,2,"/"))
