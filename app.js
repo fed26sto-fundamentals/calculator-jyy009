4bb6b57f01ba156e033b42ecbf3daac2554f4cdf
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-09-20 10:13:27 +0200

    Create event listeners for %, +/-, clear buttons

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
-let isOperatorClicked = false; 
+let isOperatorClicked = false;
 
 const add = (a, b) => a + b;
 
@@ -16,8 +16,10 @@ const calcContainer = document.querySelector(".calc-container");
 const displayContainer = document.querySelector(".display-container");
 const keypadContainer = pageContainer.querySelector(".keypad-container");
 const displayButton = displayContainer.querySelector(".display-button");
-const equal = keypadContainer.querySelector(".equal")
-
+const equal = keypadContainer.querySelector(".equal");
+const clear = keypadContainer.querySelector(".clear");
+const sign = keypadContainer.querySelector(".sign");
+const percent = keypadContainer.querySelector(".percentage")
 const operate = (a, b, oper) => {
   switch (oper) {
     case "+":
@@ -27,20 +29,19 @@ const operate = (a, b, oper) => {
     case "*":
       return multiply(a, b);
     case "/":
-      return divide(a, b);
+      // return divide(a, b)
+     const num = divide(a, b)
+     return Number(num.toFixed(10))
   }
- }
+};
 
- equal.addEventListener("click", () => {
-  const a = parseInt(firstNumber)
-  const b = parseInt(secondNumber)
-  const result = operate(a, b, operator)
+equal.addEventListener("click", () => {
+  const a = parseInt(firstNumber);
+  const b = parseInt(secondNumber);
+  const result = operate(a, b, operator);
   displayButton.textContent = result;
-  console.log(result)
- })
-
-
-
+  console.log("operator result:", result);
+});
 
 const displayNumbers = () => {
   const buttons = keypadContainer.querySelectorAll(".operand");
@@ -51,26 +52,23 @@ const displayNumbers = () => {
       console.log("button value:", buttonValue);
 
       if (!isOperatorClicked) {
-         firstNumber += buttonValue;
-      displayButton.textContent = firstNumber;
-      console.log("display (first #)", firstNumber);
+        firstNumber += buttonValue;
+        displayButton.textContent = firstNumber;
+        console.log("display (first #)", firstNumber);
       } else {
         displayButton.textContent = "";
-
         secondNumber += buttonValue;
         displayButton.textContent = secondNumber;
         console.log("display (second #)", secondNumber);
       }
-     
     });
   });
 };
 
-const displayOperator = () => {
+// const displayOperator = () => {
   const operators = keypadContainer.querySelectorAll(".operator");
   operators.forEach((oper) => {
     oper.addEventListener("click", (e) => {
-
       const operatorValue = e.target.value;
       console.log("operator value:", operatorValue);
       operator = operatorValue;
@@ -78,10 +76,35 @@ const displayOperator = () => {
 
       displayButton.textContent = operator;
       console.log("display value:", operator);
-
     });
   });
-};
+// };
+
+// function when clear button is clicked
+clear.addEventListener("click", () => {
+  isOperatorClicked = false;
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  displayButton.textContent = "";
+});
+
+// function when negate button is clicked
+sign.addEventListener("click", () => {
+  const value = displayButton.textContent
+  const negatedValue = value * -1
+  displayButton.textContent = negatedValue
+  console.log("negated value:", negatedValue)
+});
+
+// function when percentage is clicked
+percent.addEventListener("click", () => {
+  const value = displayButton.textContent
+  const percent = value / 100
+  displayButton.textContent = percent
+  console.log("percentage:", percent)
+})
+
 
 displayNumbers();
-displayOperator();
+// displayOperator();
